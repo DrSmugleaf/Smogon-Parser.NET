1bc0718c79b11bba10690efbb54e59e2fcb2dc6a
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-01-21 16:10:00 +0100

    Add missing data to SmogonAbility.cs

--- SmogonParser.NET/Parsers/Smogon/Json/SmogonAbility.cs
+++ SmogonParser.NET/Parsers/Smogon/Json/SmogonAbility.cs
@@ -1,7 +1,32 @@
-﻿namespace SmogonParser.NET.Parsers.Smogon.Json
+﻿using System.Collections.Immutable;
+using System.Text.Json.Serialization;
+
+namespace SmogonParser.NET.Parsers.Smogon.Json
 {
     public class SmogonAbility
     {
-        
+        public SmogonAbility(
+            string name,
+            string description,
+            string isNonstandard,
+            ImmutableHashSet<string> genFamily)
+        {
+            Name = name;
+            Description = description;
+            IsNonstandard = isNonstandard;
+            GenFamily = genFamily;
+        }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("isNonstandard")]
+        public string IsNonstandard { get; set; }
+
+        [JsonPropertyName("genfamily")]
+        public ImmutableHashSet<string> GenFamily { get; set; }
     }
 }
